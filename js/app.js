2a9a0fd4a8cfe365f6474b476175abaedf41dec3
megamemma <sireneviyy@gmail.com>
2026-09-07 22:21:21 +0500

    Add render function

--- js/app.js
+++ js/app.js
@@ -8,24 +8,30 @@ let data = [];
 let currentTimeframe = 'weekly';
 
 async function init() {
-    const response = await.fetch('../data.json');
+    const response = await fetch('../data.json');
     data = await response.json();
-    // render ();
+    render();
 
     document.querySelectorAll('.period-nav button').forEach(btn => {
         btn.addEventListener('click', () => {
             document.querySelector('.period-nav .is-active')?.classList.remove('is-active');
             btn.classList.add('is-active');
             currentTimeframe = btn.textContent.toLowerCase();
-            // render ();
+            render(); 
         });
     });
 }
 
-    // FUNC render (call many times)
-    // for each card, learn its category, 
-    // find and obj w/ same name in DATA
-    // take data for currentTimeframe
-    // get current hours into card's text
-    // get old hours + label(yest.,last w.,last mo.)
-// init(); 
+function render() {
+    document.querySelectorAll('.card').forEach(card => {
+        const category = card.dataset.category;
+        const entry = data.find(d => d.title.toLowerCase().replace(' ', '-' === category));
+        if (!entry) return;
+
+        const t = entry.timeframes[currentTimeframe];
+        card.querySelector('.hours-current').textContent = `${t.current}hrs`;
+        card.querySelector('.hours-past').textContent = `${previousLabel[currentTimeframe]} - ${t.previous}hrs`;
+        })
+}
+
+init();
